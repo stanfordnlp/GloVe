d9115b51a74a1d25aa3b0320ede0c6170ed56a63
Russell <russell.s.stewart@gmail.com>
2015-09-01 17:51:07 -0700

    Moved matlab files to eval/matlab.

--- eval/WordLookup.m
+++
@@ -1,9 +0,0 @@
-function index = WordLookup(InputString)
-global wordMap
-if wordMap.isKey(InputString)
-    index = wordMap(InputString);
-elseif wordMap.isKey('<unk>')
-    index = wordMap('<unk>');
-else
-    index = 0;
-end
