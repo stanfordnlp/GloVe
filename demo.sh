8dcc9aa8ba6a61aa1c2f0270b345480e91619b61
Christopher Manning <manning@cs.stanford.edu>
2015-10-24 07:29:18 -0700

    Try curl if no wget for better OS X experience.

--- demo.sh
+++ demo.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Makes programs, downloads sample data, trains a GloVe model, and then evaluates it.
+# One optional argument can specify the language used for eval script: matlab, octave or [default] python
+
 make
 if [ ! -e text8 ]; then
-  wget http://mattmahoney.net/dc/text8.zip
+  if hash wget 2>/dev/null; then
+    wget http://mattmahoney.net/dc/text8.zip
+  else
+    curl -O http://mattmahoney.net/dc/text8.zip
+  fi
   unzip text8.zip
   rm text8.zip
 fi
@@ -46,5 +53,3 @@ if [[ $? -eq 0 ]]
     fi
   fi
 fi
-
-
