f4fb7dd25155d28a99a557d0c30fd18e6840d64e
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-04-14 10:27:36 +0200

    Changed the default of the binary parameter to 0. Fixes #21.

--- src/glove.c
+++ src/glove.c
@@ -45,7 +45,7 @@ int num_threads = 8; // pthreads
 int num_iter = 25; // Number of full passes through cooccurrence matrix
 int vector_size = 50; // Word vector size
 int save_gradsq = 0; // By default don't save squared gradient values
-int use_binary = 1; // 0: save as text files; 1: save as binary; 2: both. For binary, save both word and context word vectors.
+int use_binary = 0; // 0: save as text files; 1: save as binary; 2: both. For binary, save both word and context word vectors.
 int model = 2; // For text file output only. 0: concatenate word and context vectors (and biases) i.e. save everything; 1: Just save word vectors (no bias); 2: Save (word + context word) vectors (no biases)
 real eta = 0.05; // Initial learning rate
 real alpha = 0.75, x_max = 100.0; // Weighting function parameters, not extremely sensitive to corpus, though may need adjustment for very small or very large corpora
