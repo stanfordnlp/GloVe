dde00a8b52952481c9908fc11e8890b27c993747
Russell Stewart <Russell.S.Stewart@gmail.com>
2016-04-17 14:08:37 -0700

    Added free() to 7 variables in src/glove.c.

--- src/glove.c
+++ src/glove.c
@@ -290,8 +290,11 @@ int train_glove() {
         for (a = 0; a < num_threads; a++) pthread_create(&pt[a], NULL, glove_thread, (void *)&thread_ids[a]);
         for (a = 0; a < num_threads; a++) pthread_join(pt[a], NULL);
         for (a = 0; a < num_threads; a++) total_cost += cost[a];
+        free(thread_ids);
         fprintf(stderr,"iter: %03d, cost: %lf\n", b+1, total_cost/num_lines);
     }
+    free(pt);
+    free(lines_per_thread);
     return save_params();
 }
 
@@ -316,6 +319,7 @@ int main(int argc, char **argv) {
     input_file = malloc(sizeof(char) * MAX_STRING_LENGTH);
     save_W_file = malloc(sizeof(char) * MAX_STRING_LENGTH);
     save_gradsq_file = malloc(sizeof(char) * MAX_STRING_LENGTH);
+    int result = 0;
     
     if (argc == 1) {
         printf("GloVe: Global Vectors for Word Representation, v0.2\n");
@@ -354,39 +358,44 @@ int main(int argc, char **argv) {
         printf("\t\tSave accumulated squared gradients; default 0 (off); ignored if gradsq-file is specified\n");
         printf("\nExample usage:\n");
         printf("./glove -input-file cooccurrence.shuf.bin -vocab-file vocab.txt -save-file vectors -gradsq-file gradsq -verbose 2 -vector-size 100 -threads 16 -alpha 0.75 -x-max 100.0 -eta 0.05 -binary 2 -model 2\n\n");
-        return 0;
+        result = 0;
+    } else {
+        if ((i = find_arg((char *)"-verbose", argc, argv)) > 0) verbose = atoi(argv[i + 1]);
+        if ((i = find_arg((char *)"-vector-size", argc, argv)) > 0) vector_size = atoi(argv[i + 1]);
+        if ((i = find_arg((char *)"-iter", argc, argv)) > 0) num_iter = atoi(argv[i + 1]);
+        if ((i = find_arg((char *)"-threads", argc, argv)) > 0) num_threads = atoi(argv[i + 1]);
+        cost = malloc(sizeof(real) * num_threads);
+        if ((i = find_arg((char *)"-alpha", argc, argv)) > 0) alpha = atof(argv[i + 1]);
+        if ((i = find_arg((char *)"-x-max", argc, argv)) > 0) x_max = atof(argv[i + 1]);
+        if ((i = find_arg((char *)"-eta", argc, argv)) > 0) eta = atof(argv[i + 1]);
+        if ((i = find_arg((char *)"-binary", argc, argv)) > 0) use_binary = atoi(argv[i + 1]);
+        if ((i = find_arg((char *)"-model", argc, argv)) > 0) model = atoi(argv[i + 1]);
+        if (model != 0 && model != 1) model = 2;
+        if ((i = find_arg((char *)"-save-gradsq", argc, argv)) > 0) save_gradsq = atoi(argv[i + 1]);
+        if ((i = find_arg((char *)"-vocab-file", argc, argv)) > 0) strcpy(vocab_file, argv[i + 1]);
+        else strcpy(vocab_file, (char *)"vocab.txt");
+        if ((i = find_arg((char *)"-save-file", argc, argv)) > 0) strcpy(save_W_file, argv[i + 1]);
+        else strcpy(save_W_file, (char *)"vectors");
+        if ((i = find_arg((char *)"-gradsq-file", argc, argv)) > 0) {
+            strcpy(save_gradsq_file, argv[i + 1]);
+            save_gradsq = 1;
+        }
+        else if (save_gradsq > 0) strcpy(save_gradsq_file, (char *)"gradsq");
+        if ((i = find_arg((char *)"-input-file", argc, argv)) > 0) strcpy(input_file, argv[i + 1]);
+        else strcpy(input_file, (char *)"cooccurrence.shuf.bin");
+        
+        vocab_size = 0;
+        fid = fopen(vocab_file, "r");
+        if (fid == NULL) {fprintf(stderr, "Unable to open vocab file %s.\n",vocab_file); return 1;}
+        while ((i = getc(fid)) != EOF) if (i == '\n') vocab_size++; // Count number of entries in vocab_file
+        fclose(fid);
+
+        result = train_glove();
+        free(cost);
     }
-    
-    
-    if ((i = find_arg((char *)"-verbose", argc, argv)) > 0) verbose = atoi(argv[i + 1]);
-    if ((i = find_arg((char *)"-vector-size", argc, argv)) > 0) vector_size = atoi(argv[i + 1]);
-    if ((i = find_arg((char *)"-iter", argc, argv)) > 0) num_iter = atoi(argv[i + 1]);
-    if ((i = find_arg((char *)"-threads", argc, argv)) > 0) num_threads = atoi(argv[i + 1]);
-    cost = malloc(sizeof(real) * num_threads);
-    if ((i = find_arg((char *)"-alpha", argc, argv)) > 0) alpha = atof(argv[i + 1]);
-    if ((i = find_arg((char *)"-x-max", argc, argv)) > 0) x_max = atof(argv[i + 1]);
-    if ((i = find_arg((char *)"-eta", argc, argv)) > 0) eta = atof(argv[i + 1]);
-    if ((i = find_arg((char *)"-binary", argc, argv)) > 0) use_binary = atoi(argv[i + 1]);
-    if ((i = find_arg((char *)"-model", argc, argv)) > 0) model = atoi(argv[i + 1]);
-    if (model != 0 && model != 1) model = 2;
-    if ((i = find_arg((char *)"-save-gradsq", argc, argv)) > 0) save_gradsq = atoi(argv[i + 1]);
-    if ((i = find_arg((char *)"-vocab-file", argc, argv)) > 0) strcpy(vocab_file, argv[i + 1]);
-    else strcpy(vocab_file, (char *)"vocab.txt");
-    if ((i = find_arg((char *)"-save-file", argc, argv)) > 0) strcpy(save_W_file, argv[i + 1]);
-    else strcpy(save_W_file, (char *)"vectors");
-    if ((i = find_arg((char *)"-gradsq-file", argc, argv)) > 0) {
-        strcpy(save_gradsq_file, argv[i + 1]);
-        save_gradsq = 1;
-    }
-    else if (save_gradsq > 0) strcpy(save_gradsq_file, (char *)"gradsq");
-    if ((i = find_arg((char *)"-input-file", argc, argv)) > 0) strcpy(input_file, argv[i + 1]);
-    else strcpy(input_file, (char *)"cooccurrence.shuf.bin");
-    
-    vocab_size = 0;
-    fid = fopen(vocab_file, "r");
-    if (fid == NULL) {fprintf(stderr, "Unable to open vocab file %s.\n",vocab_file); return 1;}
-    while ((i = getc(fid)) != EOF) if (i == '\n') vocab_size++; // Count number of entries in vocab_file
-    fclose(fid);
-    
-    return train_glove();
+    free(vocab_file);
+    free(input_file);
+    free(save_W_file);
+    free(save_gradsq_file);
+    return result;
 }
