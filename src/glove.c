38dc5779aeadd3d040a7267d6d921444c2b1890c
kehe <caikehe@gmail.com>
2017-11-30 02:35:40 +0000

    solve build warning issue

--- src/glove.c
+++ src/glove.c
@@ -221,7 +221,7 @@ int save_params(int nb_iter) {
         fid = fopen(vocab_file, "r");
         sprintf(format,"%%%ds",MAX_STRING_LENGTH);
         if (fid == NULL) {fprintf(stderr, "Unable to open file %s.\n",vocab_file); return 1;}
-	if (write_header) fprintf(fout, "%ld %d\n", vocab_size, vector_size);
+	if (write_header) fprintf(fout, "%lld %d\n", vocab_size, vector_size);
         for (a = 0; a < vocab_size; a++) {
             if (fscanf(fid,format,word) == 0) return 1;
             // input vocab cannot contain special <unk> keyword
